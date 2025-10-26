a01657090938b437fee11620eb9e69abf768ede4
Macharia11 <stevieskaka3096@gmail.com>
2025-10-26 21:33:31 +0300

    middleware

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs"
-
+//sign in
 export default function SignInPage() {
   return (
     <div className="container flex min-h-[calc(100vh-4rem)] items-center justify-center py-12">
